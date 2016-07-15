d879e2db809a36ffbf03156159135720d39e9440
Christian Urcuqui <ulcamilo@gmail.com>
2016-07-15 15:21:31 -0500

    Add project files.

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                new Label {
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    Text = "Welcome to Xamarin Forms!"
+                }
+            }
+            };
+        }
+    }
+}
