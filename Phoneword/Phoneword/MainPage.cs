7d76f63530372d1e35b023f3b9e7db5effd2af14
Christian Urcuqui <ulcamilo@gmail.com>
2016-07-15 16:09:51 -0500

    edx example

--- Phoneword/Phoneword/MainPage.cs
+++ Phoneword/Phoneword/MainPage.cs
@@ -9,18 +9,56 @@ namespace Phoneword
 {
     public class MainPage : ContentPage
     {
+        Entry phoneNumberText;
+        Button btnTranslate;
+        Button btnCall;
+        string translatedNumber;
+
         public MainPage()
         {
-            Content = new StackLayout
+            StackLayout panel = new StackLayout
             {
-                VerticalOptions = LayoutOptions.Center,
-                Children = {
-                new Label {
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = "Welcome to Xamarin Forms!"
-                }
-            }
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation =  StackOrientation.Vertical,
+                Spacing = 15,
             };
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+            panel.Children.Add(btnTranslate = new Button
+            {
+                Text = "Translate"
+            });
+            panel.Children.Add(btnCall = new Button
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+            btnTranslate.Clicked += OnTranslate;
+
+            this.Content = panel;
+
+        }
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                btnCall.IsEnabled = true;
+                btnCall.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                btnCall.IsEnabled = false;
+                btnCall.Text = "Call";
+            }
         }
     }
 }
